59bd592f581d7077033a5ac015fa311d09f660b0
David <david19kerrigan@gmail.com>
2024-10-17 13:17:20 -0400

    auto commit

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /* interval between updates (in ms) */ const unsigned int interval = 15000;
 
 /* text to show if no value can be retrieved */
-static const char unknown_str[] = "n/a";
+static const char unknown_str[] = "100";
 
 /* maximum output string length */
 #define MAXLEN 2048
@@ -65,6 +65,6 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function format          argument */
 	//{ wifi_essid, " 󰖩 %s | ",           "wlan0" },
-	//{ battery_perc, " 0.%s ",           "BAT0" },
+	{ battery_perc, " %s%% ",           "BAT0" },
 	{ datetime, "%s",           " %a %b %d %Y  %I:%M%p " },
 };
